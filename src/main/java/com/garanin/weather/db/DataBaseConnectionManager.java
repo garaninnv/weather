54d1e2db58e7086bcdae4e4fdb01d6e730f09d4e
GaraninNV <nikolas-ua@mail.ru>
2023-07-06 00:41:18 +0300

    1.Настройка зависимостей 2.Подключение к БД 3.Создание основ для форм регистрации и авторизации

---
+++ src/main/java/com/garanin/weather/db/DataBaseConnectionManager.java
@@ -0,0 +1,28 @@
+package com.garanin.weather.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnectionManager {
+    private Connection connection;
+    private final String user = "postgres";
+    private final String password = "123";
+
+    public DataBaseConnectionManager(String jdbcURL) throws SQLException, ClassNotFoundException {
+
+        Class.forName("org.postgresql.Driver");
+        this.connection = DriverManager.getConnection(jdbcURL, user, password);
+        connection = null;
+        try {
+            connection = DriverManager.getConnection(jdbcURL, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
