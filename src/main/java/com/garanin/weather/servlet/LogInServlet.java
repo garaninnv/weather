97401517c159e7efd65ae29ebb550624275fc6e6
GaraninNV <nikolas-ua@mail.ru>
2023-08-08 00:02:16 +0300

    - При авторизации если пользователь не найден, выводится сообщение. Введеный логин повторно вводить не требуется

--- src/main/java/com/garanin/weather/servlet/LogInServlet.java
+++ src/main/java/com/garanin/weather/servlet/LogInServlet.java
@@ -1,12 +1,15 @@
 package com.garanin.weather.servlet;
 
 import com.garanin.weather.dao.UserDAO;
+import com.garanin.weather.util.ThymeleafUtil;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
 
@@ -16,7 +19,7 @@ public class LogInServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("authorizationForm/logIn.html");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("view/login.html");
         requestDispatcher.forward(req, resp);
     }
 
@@ -25,11 +28,16 @@ public class LogInServlet extends HttpServlet {
         String user = req.getParameter("login");
         String pas = req.getParameter("password");
 
-        if (!user.equals("") && !pas.equals("")) {
+        if (user != null && pas != null && !user.equals("") && !pas.equals("")) {
             if (userDAO.findUser(user, pas, resp)) {
                 resp.sendRedirect("/weather/index");
             } else {
-                resp.sendRedirect("/weather/login");
+                boolean nouser = true;
+                TemplateEngine engine = ThymeleafUtil.buildTemplateEngine(req.getServletContext());
+                WebContext context = ThymeleafUtil.buildWebContext(req, resp, getServletContext());
+                context.setVariable("login", user);
+                context.setVariable("nouser", nouser);
+                engine.process("login", context, resp.getWriter());
             }
         }
     }
--- src/main/java/com/garanin/weather/servlet/RegistrationServlet.java
+++ src/main/java/com/garanin/weather/servlet/RegistrationServlet.java
@@ -14,7 +14,7 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("authorizationForm/registration.html");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("view/registration.html");
         requestDispatcher.forward(req, resp);
     }
 
